da3bb05e2b51ef17f8cdb825f994366561c132cf
LS4Tonio <toniolesniper@gmail.com>
2014-11-05 11:18:50 +0100

    Application created

---
+++ Mascotte Serveur/Mascotte Serveur/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Mascotte_Serveur
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
