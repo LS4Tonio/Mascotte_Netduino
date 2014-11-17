a0c82defa3a9ae660e6e46da53d8bfa0dfd23c45
LS4Tonio <toniolesniper@gmail.com>
2014-11-17 09:33:12 +0100

    Main window created

--- Mascotte Serveur/Mascotte Serveur/MainWindow.xaml.cs
+++ Mascotte Serveur/Mascotte Serveur/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,6 +24,24 @@ namespace Mascotte_Serveur
         public MainWindow()
         {
             InitializeComponent();
+
+            NetDevices nd = new NetDevices();
+
+            // Get my PC IP address
+            this.ipandmacaddresses.Text += String.Format("\nMy IP : {0}\n", nd.GetIPAddress());
+            // Get My PC MAC address
+            this.ipandmacaddresses.Text += String.Format("My MAC: {0}\n", nd.GetMacAddress());
+            // Get all devices on network
+            Dictionary<IPAddress, PhysicalAddress> all = nd.GetAllDevicesOnLAN();
+
+            foreach (KeyValuePair<IPAddress, PhysicalAddress> kvp in all)
+            {
+                this.ipandmacaddresses.Text += String.Format("IP : {0} --- MAC : {1}\n", kvp.Key, kvp.Value);
+            }
+
+            byte[] macAddress = new byte[]{Byte.Parse("5C"), Byte.Parse("86"), Byte.Parse("4A"), Byte.Parse("00"), Byte.Parse("E7"), Byte.Parse("28")};
+            KeyValuePair<IPAddress, PhysicalAddress> netduino = all.Single<KeyValuePair<IPAddress, PhysicalAddress>>(x => x.Value.Equals(macAddress));
+            this.ipandmacaddresses.Text += String.Format("Netduino : IP : {0}\n", netduino.Key);
         }
     }
 }
---
+++ Rover Tests/Rover Tests/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+
+namespace Rover_Tests
+{
+    public class Program
+    {
+        public static void blinkLed(OutputPort pin, int nb)
+        {
+            for (int i = 0; i < nb; i++)
+            {
+                pin.Write(true);
+                Thread.Sleep(500);
+                pin.Write(false);
+                Thread.Sleep(500);
+            }
+        }
+
+        public static void Main()
+        {
+            OutputPort pwm = new OutputPort(Pins.GPIO_PIN_D0, false);
+            OutputPort dir = new OutputPort(Pins.GPIO_PIN_D1, false);
+            OutputPort current = new OutputPort(Pins.GPIO_PIN_A0, false);
+            OutputPort onBoardLed = new OutputPort(Pins.ONBOARD_LED, false);
+
+            while (true)
+            {
+                blinkLed(onBoardLed, 5);
+                //pwm.Write(true);
+            }
+        }
+    }
+}
