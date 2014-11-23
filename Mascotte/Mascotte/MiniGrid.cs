1800c0f0875c5f0ccd4c8aae8c82ab20aebc3b5a
myzaki <mougel@intechinfo.fr>
2014-11-23 16:31:10 +0100

    Algo Continuing, ni idea how to manipulate the bytes

---
+++ Mascotte/Mascotte/MiniGrid.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mascotte
+{
+    public class MiniGrid : Grid
+    {
+        byte[][] _datas;
+        public MiniGrid(byte[][] datas)
+            : base(datas)
+        {
+            _datas = datas;
+        }
+        public byte[][] DatasInMiniMap { get; set; }
+        public int MapPosX { get; set; }
+        public int MapPosY { get; set; }
+        public byte MapConfidenceIndice { get; set; }
+        
+
+        public void Synchronize(int[][] positions)
+        {
+
+        }
+    }
+}
