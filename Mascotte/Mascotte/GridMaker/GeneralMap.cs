59e4028af5dc9904da2c2af1979e8951549047a7
LS4Tonio <toniolesniper@gmail.com>
2014-12-05 15:59:13 +0100

    GenaralMap tests bug

--- Mascotte/Mascotte/GridMaker/GeneralMap.cs
+++ Mascotte/Mascotte/GridMaker/GeneralMap.cs
@@ -8,23 +8,54 @@ namespace Mascotte.GridMaker
 {
     public class GeneralMap
     {
-        MiniGrid minimap;
+        private enum CaseState
+        {
+            Moving = 0 << 7,
+            Stable = 1 << 7,
+            Validated = 1 << 8,
+            OnValidating = 0 << 8
+        }
+
+        MiniGrid _minimap;
+        byte[][] _gridContent;
         protected const int GENERAL_MAP_SIZE = 200;
         
         public GeneralMap()
         {
-            byte[][] datas = new byte[8][];
+            int size = 1024;
+            int size2 = 8;
 
+            byte[][] datas = new byte[size2][];
             for (int i = 0; i < datas.Length; i++)
-                datas[i] = new byte[8];
+                datas[i] = new byte[size2];
 
-            minimap = new MiniGrid(datas, this.GridContent);
+            _gridContent = new byte[size][];
+            for (int i = 0; i < _gridContent.Length; i++)
+            {
+                _gridContent[i] = new byte[size];
+                for (int j = 0; j < _gridContent[i].Length; j++)
+                {
+                    _gridContent[i][j] = 0;
+                }
+            }
+
+            _minimap = new MiniGrid(datas, this.GridContent);
         }
 
         /// <summary>
         /// Gets or set map content
         /// </summary>
-        public byte[][] GridContent { get; set; }
+        public byte[][] GridContent
+        {
+            get { return _gridContent; }
+        }
+        /// <summary>
+        /// Gets minimap
+        /// </summary>
+        public MiniGrid Minimap
+        {
+            get { return _minimap; }
+        }
 
         /// <summary>
         /// Merge present Minigrid with parentMap 
@@ -33,18 +64,18 @@ namespace Mascotte.GridMaker
         /// </summary>
         public void Synchronize()
         {
-            for (int i = 0; i < minimap.DatasInMiniMap.Length; i++)
+            for (int i = 0; i < _minimap.DatasInMiniMap.Length; i++)
             {
-                for (int j = 0; j < minimap.DatasInMiniMap[i].Length; i++)
+                for (int j = 0; j < _minimap.DatasInMiniMap[i].Length; i++)
                 {
-                    if (GridContent[i + minimap.MapPosY][j + minimap.MapPosX] < 63)
+                    if (GridContent[i + _minimap.MapPosY][j + _minimap.MapPosX] < 63)
                     {
-                        GridContent[i + minimap.MapPosY][j + minimap.MapPosX] += minimap.DatasInMiniMap[i][j];
+                        GridContent[i + _minimap.MapPosY][j + _minimap.MapPosX] += _minimap.DatasInMiniMap[i][j];
                         // When adding is done, scan new value
-                        if (GridContent[i + minimap.MapPosY][j + minimap.MapPosX] >= 63)
-                            GridContent[i + minimap.MapPosY][j + minimap.MapPosX] += 1 << 7;
+                        if (GridContent[i + _minimap.MapPosY][j + _minimap.MapPosX] >= 63)
+                            GridContent[i + _minimap.MapPosY][j + _minimap.MapPosX] += 1 << 7;
                     }
-                    else if(GridContent[i + minimap.MapPosY][j + minimap.MapPosX] > 63 && GridContent[i + minimap.MapPosY][j + minimap.MapPosX] > minimap.DatasInMiniMap[i][j])
+                    else if(GridContent[i + _minimap.MapPosY][j + _minimap.MapPosX] > 63 && GridContent[i + _minimap.MapPosY][j + _minimap.MapPosX] > _minimap.DatasInMiniMap[i][j])
                         ThrowMessage("L'Objet anciennement validé semble avoir bougé");
                 }
             }
@@ -62,14 +93,9 @@ namespace Mascotte.GridMaker
         /// </summary>
         /// <param name="posX"></param>
         /// <param name="posY"></param>
-        private void ValidateObject(int posX, int posY) { } // Not sure this func will
-        
-        private enum CaseState
+        private void ValidateObject(int posX, int posY)
         {
-            Moving = 0 << 7,
-            Stable = 1 << 7,
-            Validated = 1 << 8,
-            OnValidating = 0 << 8
-        }
+            //TO DO
+        } // Not sure this func will
     }
 }
--- Mascotte/Tests/GeneralMapTests.cs
+++ Mascotte/Tests/GeneralMapTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using Mascotte;
+using Mascotte.GridMaker;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +15,64 @@ namespace Tests
         [Test]
         public void synchronizeTest()
         {
+            GeneralMap gm = new GeneralMap();
+
+            addDatasInMinimap(gm.Minimap);
+
+            for(int i=0; i<8; i++)
+            {
+                for(int j=0; j<8; j++)
+                {
+                    Assert.AreEqual(gm.GridContent[i][j], 0, "BeforeTest");
+                }
+            }
+
+            gm.Synchronize();
+
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                            Assert.AreEqual(gm.GridContent[i][j], 0, "i && j pair");
+                        else
+                            Assert.AreEqual(gm.GridContent[i][j], 63, "i pair && j impair");
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                            Assert.AreEqual(gm.GridContent[i][j], 127, "i impair && j pair");
+                        else
+                            Assert.AreEqual(gm.GridContent[i][j], 255, "i && j impair");
+                    }
+                }
+            }
+        }
+
+        public void addDatasInMinimap(MiniGrid grid)
+        {
+            for (int i = 0; i < grid.DatasInMiniMap.Length; i++)
+            {
+                for (int j = 0; j < grid.DatasInMiniMap[i].Length; j++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        if (j % 2 == 0)
+                            grid.DatasInMiniMap[i][j] = 0;
+                        else
+                            grid.DatasInMiniMap[i][j] = 63;
+                    }
+                    else
+                    {
+                        if (j % 2 == 0)
+                            grid.DatasInMiniMap[i][j] = 127;
+                        else
+                            grid.DatasInMiniMap[i][j] = 255;
+                    }
+                }
+            }
         }
     }
 }
