6b099ad55745480726563ddf6cff28ed463f1173
myzaki <mougel@intechinfo.fr>
2014-12-16 14:31:23 +0100

    receiving network

--- Mascotte/ServerFront/Server.cs
+++ Mascotte/ServerFront/Server.cs
@@ -7,6 +7,7 @@ using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
+using System.Net.Sockets;
 
 namespace ServerFront
 {
